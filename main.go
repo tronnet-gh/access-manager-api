0b4199c7982a1e961e076426a2986959dd34c611
cyrus <vsasaoka@gmail.com>
2024-05-24 20:53:34 +0000

    post echo request

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"bytes"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
@@ -8,6 +13,13 @@ func main() {
 		c.JSON(200, gin.H{
 			"version": "0.0.1",
 		})
+	})
+
+	router.POST("/echo", func(c *gin.Context) {
+		buf := new(bytes.Buffer)
+		buf.ReadFrom(c.Request.Body)
+		data := buf.String()
+		fmt.Println(data)
 	})
 	router.Run("0.0.0.0:80") // listen and serve on 0.0.0.0:8080
 }
